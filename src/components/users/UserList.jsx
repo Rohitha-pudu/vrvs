f83defb74b85917b6f748eae8d29b0b10e6d8bbc
Rohitha Pudu <rohithapudu@gmail.com>
2024-11-25 00:29:37 +0530

    RBAC system

--- src/components/users/UserList.jsx
+++ src/components/users/UserList.jsx
@@ -298,10 +298,10 @@ const UserList = () => {
 
       {/* Deletion confirmation modal */}
       {isDeleting && (
-        <div className="fixed inset-0 flex justify-center items-center bg-gray-800 bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm mx-auto">
-            <p className="text-xl font-semibold mb-4">Are you sure you want to delete this user?</p>
-            <div className="flex justify-between">
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30 z-50">
+              <div className="bg-white p-4 rounded-lg shadow-lg max-w-sm w-full text-center">
+                <h3 className="text-xl">Are you sure you want to delete this user?</h3>
+                <div className="flex  justify-between gap-4 mt-4">
               <button
                 onClick={confirmDelete}
                 className="bg-red-500 text-white px-4 py-2 rounded"
@@ -316,9 +316,10 @@ const UserList = () => {
               </button>
             </div>
           </div>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      
   );
 };
 
--- src/context/RBACContext.jsx
+++ src/context/RBACContext.jsx
@@ -5,9 +5,10 @@ export const RBACContext = createContext();
 export const RBACProvider = ({ children }) => {
   const [users, setUsers] = useState([
     { id: 1, name: 'John Doe', role: 'Admin', status: 'Invite Pending', email: 'john.doe@example.com', lastActivity: '5 days back' },
-    { id: 2, name: 'Jane Smith', role: 'Editor', status: 'Invite Pending', email: 'jane.smith@example.com', lastActivity: '5 days back' },
-    { id: 3, name: 'Riya Ali', role: 'Admin', status: 'Invite Accepted', email: 'riya.doe@example.com', lastActivity: '5 days back' },
-    { id: 4, name: 'Jack Smith', role: 'Editor', status: 'Invite Expired', email: 'jakee.smith@example.com', lastActivity:'5 days back' }
+    { id: 2, name: 'Jane Smith', role: 'Editor', status: 'Invite Pending', email: 'jane.smith@example.com', lastActivity: '15 days back' },
+    { id: 3, name: 'Riya Ali', role: 'Admin', status: 'Invite Accepted', email: 'riya.doe@example.com', lastActivity: '7 days back' },
+    { id: 4, name: 'Jack Smith', role: 'Editor', status: 'Invite Expired', email: 'jakee.smith@example.com', lastActivity:'3 days back' },
+    { id: 5, name: 'Aurora', role: 'Admin', status: 'Invite Pending', email: 'aurora13@example.com', lastActivity:'1 days back' }
   ]);
 
   const [roles, setRoles] = useState([
